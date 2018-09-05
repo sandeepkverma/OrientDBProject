97004b6373a08ea53423b9b971ac18fa376d22f8
sandeep <sandeep.k@mapmyindia.com>
2018-09-05 19:00:46 +0530

    jre change

--- OTest/src/main/java/com/san/Test.java
+++ OTest/src/main/java/com/san/Test.java
@@ -10,14 +10,13 @@ import com.san.util.OrientConnection;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("sout");
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		System.out.println("connecting...........");
 		OrientConnection orientConnection = ctx.getBean(OrientConnection.class);
 		
 		
-		try(ODatabaseSession db = orientConnection.getOrientDatabaseSession();){
+		try(ODatabaseSession db = orientConnection.getOrientDatabaseSession();){ 
 			String query = "SELECT from User";
 		    OResultSet rs = db.query(query);
 
