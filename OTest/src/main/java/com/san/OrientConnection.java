33f63b2547e1975704a8f77a2eebc781c090181d
sandeep <sandeep.k@mapmyindia.com>
2018-09-05 18:21:57 +0530

    first commit

---
+++ OTest/src/main/java/com/san/OrientConnection.java
@@ -0,0 +1,23 @@
+package com.san;
+
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+
+public class OrientConnection {
+	private OrientDB orient;
+	private ODatabaseSession orientDatabaseSession;
+	public OrientConnection() {}
+	public OrientConnection(String url,String databaseName,String userName,String password) {
+		orient = new OrientDB(url, OrientDBConfig.defaultConfig());
+		orientDatabaseSession = orient.open(databaseName, userName, password);
+	}
+	
+	public OrientDB getOrient() {
+		return orient;
+	}
+	public ODatabaseSession getOrientDatabaseSession() {
+		
+		return orientDatabaseSession;
+	}
+}
---
+++ OTest/src/main/java/com/san/Test.java
@@ -0,0 +1,31 @@
+package com.san;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.sql.executor.OResult;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sout");
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		System.out.println("connecting...........");
+		OrientConnection orientConnection = ctx.getBean(OrientConnection.class);
+		
+		
+		try(ODatabaseSession db = orientConnection.getOrientDatabaseSession();){
+			String query = "SELECT from User";
+		    OResultSet rs = db.query(query);
+
+		    while (rs.hasNext()) {
+		      OResult item = rs.next();
+		      System.out.println("name: " + item.getProperty("name"));
+		    }
+
+		}
+	}
+
+}
